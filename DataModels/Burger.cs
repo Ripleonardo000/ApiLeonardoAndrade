25d72e0726077b8fe8d3344170b29277f90f87fa
Ripleo000 <leonardoandrade2003.197@gmail.com>
2024-05-08 11:40:33 -0500

    Scaffold-DbContext

---
+++ DataModels/Burger.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ApiLeonardoAndrade.DataModels;
+
+public partial class Burger
+{
+    public int BurgerId { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public bool WithCheese { get; set; }
+
+    public decimal Costo { get; set; }
+
+    public virtual ICollection<Promo> Promos { get; set; } = new List<Promo>();
+}
---
+++ DataModels/Promo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ApiLeonardoAndrade.DataModels;
+
+public partial class Promo
+{
+    public int PromoId { get; set; }
+
+    public string? Descripcion { get; set; }
+
+    public int FechaPromo { get; set; }
+
+    public int BurgerId { get; set; }
+
+    public virtual Burger Burger { get; set; } = null!;
+}
